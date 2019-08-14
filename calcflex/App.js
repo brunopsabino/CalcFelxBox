c43c1c0c2f04b69b660cf4d4d11229c92645949e
Bruno Sabino <bruno.p.sabino@gmail.com>
2019-08-13 23:06:42 -0300

    Estrutura inicial do layout

--- calcflex/App.js
+++ calcflex/App.js
@@ -2,9 +2,34 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const col1Buttons = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    [',', '0', '='],
+  ]
+
+  const col2Buttons = ['C', 'x', '÷', '+', '-']
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.display}>Display</Text>
+      <Text style={styles.result}>Result</Text>
+      <View style={styles.buttons}>
+        <View style={styles.col1}>
+          { col1Buttons.map((line, ind) => <View key={ind} style={styles.line}>
+            { line.map(op => <View key={op} style={styles.btn}>
+              <Text style={styles.btnText}>{op}</Text>
+            </View>  )}
+          </View>
+          )}
+        </View>
+        <View style={styles.col2}>
+          { col2Buttons.map(op => <View key={op} style={styles.btn}>
+            <Text style={styles.btnText}>{op}</Text>
+          </View>  )}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +38,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  display: {
+    flex: 1,
+    backgroundColor: '#EFEFEF',
+    fontSize: 80,
+    textAlign: 'right',
+    paddingTop: 30,
+    paddingRight: 10
+  },
+  result: {
+    flex: 0.4,
+    backgroundColor: '#EFEFEF',
+    fontSize: 40,
+    textAlign: 'right',
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10
+  },
+  buttons: {
+    flex: 5,
+    flexDirection: 'row'
+  },
+  col1: {
+    flex: 3,
+    backgroundColor: 'gray'
+  },
+  line: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  btnText: {
+    fontSize: 50,
+    textAlign: 'center'
+  },
+  col2: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  col:{
+    flexDirection: 'column'
+  }
+
 });
